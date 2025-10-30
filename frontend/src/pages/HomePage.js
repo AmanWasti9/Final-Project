a0eb2bb6f09c40f67721e0593a12a575bb21191a
Syed Amanullah Wasti <126661086+AmanWasti9@users.noreply.github.com>
2025-10-30 14:06:32 +0500

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import { Container } from "@mui/material";
 import FeatSlider from "../components/FeatSlider/FeatSlider";
+import Card from "../components/Card/Card";
 import NewHero from "../components/NewHero/NewHero";
 // import Signup from "../components/Signup/form";
 
@@ -9,7 +10,9 @@ export default function HomePage() {
   return (
     <Container>
       <NewHero />
-      {/* <Hero /> */}
+      <br />
+      <br />
+      <Card /> 
       <br />
       <br />
       <FeatSlider />
